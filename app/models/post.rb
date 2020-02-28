8597ec398ba87ed6727d334ea516503bf42d3975
Kasuhira <hheflerdevelopment@gmail.com>
2020-02-28 15:49:26 -0300

    Fixed post issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence:true, length { in 5..30}
+  validates :title, presence:true, length: { in: 5..30 }
   belongs_to :user
   has_many :comments
 end
